8c5e13a3a3e647e251467baa5a8051ffd51d268e
bernhste <s.bernhardsgruetter@proton.me>
2026-05-18 12:11:45 +0200

    added test for NativeFallbackDecoder

--- frooky/agent/native/decoders/nativeFallbackDecoder.test.ts
+++ frooky/agent/native/decoders/nativeFallbackDecoder.test.ts
@@ -4,8 +4,13 @@ import { NativeFallbackDecoder } from "./nativeFallbackDecoder";
 describe("NativeFallbackDecoder", () => {
   describe("decode()", () => {
     it("should decode a unknown decodable to its address as string", () => {
-      const decoder = new NativeFallbackDecoder({ type: "customStruct", settings: DEFAULT_DECODER_SETTINGS });
-      expect(decoder.decode(ptr(0x12345678))).toEqual({ type: "customStruct", value: "0x12345678" });
+      const decoder = new NativeFallbackDecoder({ type: "customStructType", settings: DEFAULT_DECODER_SETTINGS });
+      expect(decoder.decode(ptr(0x12345678))).toEqual({ type: "customStructType", value: "0x12345678" });
+    });
+
+    it("should decode a unknown named decodable to its address as string", () => {
+      const decoder = new NativeFallbackDecoder({ type: "customStructType", name: "customStructName", settings: DEFAULT_DECODER_SETTINGS });
+      expect(decoder.decode(ptr(0x12345678))).toEqual({ type: "customStructType", name: "customStructName", value: "0x12345678" });
     });
   });
 });
