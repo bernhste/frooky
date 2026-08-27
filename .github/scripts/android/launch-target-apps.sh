5461d4bc4b8e19e213bc2e09b60c071b5d3927c6
bernhste <s.bernhardsgruetter@proton.me>
2026-08-27 23:30:41 +0000

    update github workflows

--- .github/scripts/android/launch-target-apps.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -eo pipefail
-
-for apk in target-apps-android/*.apk; do
-  TARGET_APP=$(basename "$apk" .apk)
-  APP_ID="${TARGET_APP//-/_}.frooky.target.app"
-  echo "=== Launching $APP_ID ==="
-  adb shell am start -n "$APP_ID/org.owasp.mastestapp.MainActivity"
-done
