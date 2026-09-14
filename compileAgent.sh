97386ca5fa8eeda8eacd623d38388ab6d9130139
bernhste <s.bernhardsgruetter@proton.me>
2026-09-14 13:09:25 +0000

    removed ios stuff because it is not yet relevant

--- compileAgent.sh
+++ compileAgent.sh
@@ -7,11 +7,9 @@ npm ci
 case "$1" in
     --prod)
         npm run build:prod:android
-        npm run build:prod:ios
         ;;
     --dev)
         npm run build:dev:android
-        npm run build:dev:ios
         ;;
     *)
         echo "Usage: $0 {--prod|--dev}"
