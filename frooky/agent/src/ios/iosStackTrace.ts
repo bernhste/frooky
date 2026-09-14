ce52bf00ebb61470d3753b31ba38c1a5a88d2899
bernhste <s.bernhardsgruetter@proton.me>
2026-09-14 12:52:06 +0000

    remove ios not to confuse claude

--- frooky/agent/src/ios/iosStackTrace.ts
+++
@@ -1,7 +0,0 @@
-import { PlatformStackTrace } from "../shared/platformStackTrace";
-
-export const IosStackTrace: PlatformStackTrace = {
-  build(limit: number, stackTraceFilter?: string[], ctx?: CpuContext): string[] {
-    return [];
-  },
-};
